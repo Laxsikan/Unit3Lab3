f0a76f7e3ba76c0bc32cbe9f1217afc0abb9b15f
Laxsikan <manoluxsikan@gmail.com>
2023-02-21 11:49:41 -0500

    first commit

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -1,27 +1,44 @@
-
-
-using UnitTestEx;
 using Xunit;
+using MyAverageLib;
 
-namespace UnitTestEXTest;
+namespace MyAverageLibTest;
 
 public class UnitTest1
 {
     [Fact]
-    public void TestMin6_17()
-    {
-        double expected = 6;
-        double actual = UnitTestEx.Class1.Min(17,6);
+    public void TestAverageOf2and2() {
+        double a = 2;
+        double b = 2;
+        double expected = 2;
 
-        Assert.Equal(expected,actual);
+        //act
+        double actual =  Average.MyAvg1(a, b);
+
+        //assert
+        Assert.Equal(expected, actual);
     }
 
     [Fact]
-    public void TestMin6_17_3()
-    {
+    public void TestAverageOf2and3and4() {
+        double a = 2;
+        double b = 3;
+        double c = 4;
         double expected = 3;
-        double actual = UnitTestEx.Class1.Min(17,6,3);
+        
+        double actual =  Average.MyAvg1(a, b, c);
+        
+        Assert.Equal(expected, actual);
+    }
 
-        Assert.Equal(expected,actual);
+    [Fact]
+    public void TestAverageOf2and3and4fail() {
+        double a = 2;
+        double b = 3;
+        double c = 4;
+        double expected = 4;
+        
+        double actual =  Average.MyAvg1(a, b, c);
+        
+        Assert.Equal(expected, actual);
     }
 }
